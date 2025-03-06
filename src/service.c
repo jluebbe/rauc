78f3ece998c0d83a7104f1df9509e3e70925b16c
Jan Luebbe <jlu@pengutronix.de>
2025-03-06 16:44:24 +0100

    WIP

--- src/service.c
+++ src/service.c
@@ -11,6 +11,7 @@
 #include "install.h"
 #include "manifest.h"
 #include "mark.h"
+#include "nbd.h"
 #include "rauc-installer-generated.h"
 #include "service.h"
 #include "status_file.h"
@@ -676,134 +677,24 @@ static gboolean r_on_signal(gpointer user_data)
 typedef struct {
 	GSource source;
 
+	gboolean installation_running;
+
 	guint64 attempt_count;
 	guint64 recent_error_count; /* since last success */
-
 	gint64 last_attempt_time; /* monotonic */
 	gint64 last_success_time; /* monotonic */
 	gchar *last_error_message;
-	RaucManifest *manifest; /* from the last successful attempt */
 	gboolean update_available;
 	gchar *summary;
-	gboolean update_failed;
+	gchar *attempted_hash; /* manifest hash of the attempted update */
+
+	/* from the last successful attempt */
+	RaucManifest *manifest;
+	guint64 bundle_size;
+	gchar *bundle_effective_url;
+	guint64 bundle_modified_time;
+	gchar *bundle_etag;
 } RPollSource;
-
-static gboolean poll_fetch(RPollSource *poll_source, GError **error)
-{
-	GError *ierror = NULL;
-
-	/* fetch manifest */
-	g_auto(RaucBundleAccessArgs) access_args = {0};
-	access_args.http_info_headers = assemble_info_headers(NULL);
-
-	g_autoptr(RaucBundle) bundle = NULL;
-	if (!check_bundle(r_context()->config->poll_source, &bundle, CHECK_BUNDLE_DEFAULT, &access_args, &ierror)) {
-		g_propagate_error(error, ierror);
-		return FALSE;
-	}
-
-	if (!bundle->manifest) {
-		g_message("polling failed: no manifest found");
-		return FALSE;
-	}
-
-	g_clear_pointer(&poll_source->manifest, free_manifest);
-	poll_source->manifest = g_steal_pointer(&bundle->manifest);
-
-	return TRUE;
-}
-
-static gboolean poll_check_available(RPollSource *poll_source, GError **error)
-{
-	/* decide if we need to install (using system version?) */
-	g_message("polling: system-version=%s bundle=version=%s",
-			r_context()->system_version, poll_source->manifest->update_version);
-
-	/* TODO check install_if */
-
-	if (g_strcmp0(r_context()->system_version, poll_source->manifest->update_version) != 0) {
-		r_replace_strdup(&poll_source->summary, "update available: different system version");
-		return TRUE;
-	}
-
-	r_replace_strdup(&poll_source->summary, "no update available");
-	return FALSE;
-}
-
-static gboolean poll_install(RPollSource *poll_source, GError **error)
-{
-	RaucInstallArgs *args = install_args_new();
-	g_autofree gchar *message = NULL;
-	gboolean res;
-
-	g_return_val_if_fail(poll_source, FALSE);
-	g_return_val_if_fail(poll_source->manifest, FALSE);
-	g_return_val_if_fail(poll_source->manifest->hash, FALSE);
-	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
-
-	args->name = g_strdup(r_context()->config->poll_source);
-	args->notify = service_install_notify; /* FIXME? */
-	args->cleanup = service_install_cleanup; /* FIXME? */
-	/* lock bundle via manifest hash */
-	args->require_manifest_hash = g_strdup(poll_source->manifest->hash);
-
-	r_installer_set_operation(r_installer, "installing");
-	res = install_run(args);
-	if (!res) {
-		message = g_strdup("Failed to launch install thread");
-		args->status_result = 1;
-		goto out;
-	}
-	args = NULL;
-
-out:
-	g_clear_pointer(&args, install_args_free);
-	if (res) {
-	} else {
-		r_installer_set_operation(r_installer, "idle");
-	}
-
-	return TRUE;
-
-	/* TODO trigger installation, which may reboot at the end */
-
-}
-
-static gboolean poll_step(RPollSource *poll_source, GError **error)
-{
-	GError *ierror = NULL;
-
-	if (!poll_fetch(poll_source, &ierror)) {
-		g_propagate_error(error, ierror);
-		return FALSE;
-	}
-	/* TODO store cache info */
-
-	g_clear_pointer(&poll_source->summary, g_free);
-	poll_source->update_available = FALSE;
-	gboolean available = poll_check_available(poll_source, &ierror);
-	if (ierror) {
-		g_propagate_prefixed_error(error, ierror, "availability check failed: ");
-		return FALSE;
-	} else if (!available) {
-		return TRUE;
-	} else {
-		poll_source->update_available = TRUE;
-	}
-
-	/* TODO only retry if manifest hash changes? */
-	if (!poll_source->update_failed) {
-		if (!poll_install(poll_source, &ierror)) {
-			g_propagate_prefixed_error(error, ierror, "starting installation failed: ");
-			poll_source->update_failed = TRUE;
-			return FALSE;
-		} else {
-			poll_source->update_failed = FALSE;
-		}
-	}
-
-	return TRUE;
-}
 
 typedef enum {
 	POLL_DELAY_NORMAL = 0,
@@ -848,6 +739,168 @@ static void poll_reschedule(RPollSource *poll_source, RPollDelay delay)
 	g_message("scheduled next poll in: %s", duration_str);
 }
 
+static gboolean poll_fetch(RPollSource *poll_source, GError **error)
+{
+	GError *ierror = NULL;
+
+	g_return_val_if_fail(poll_source, FALSE);
+
+	/* fetch manifest */
+	g_auto(RaucBundleAccessArgs) access_args = {0};
+	access_args.http_info_headers = assemble_info_headers(NULL);
+	if (poll_source->bundle_etag) {
+		g_ptr_array_add(access_args.http_info_headers, g_strdup_printf("If-None-Match: %s", poll_source->bundle_etag));
+	}
+
+	g_autoptr(RaucBundle) bundle = NULL;
+	if (!check_bundle(r_context()->config->poll_source, &bundle, CHECK_BUNDLE_DEFAULT, &access_args, &ierror)) {
+		if (g_error_matches(ierror, R_NBD_ERROR, R_NBD_ERROR_NO_CONTENT)) {
+			g_message("polling: no bundle available");
+			/* TODO update summary? */
+			return TRUE; /* FIXME should this be an error? */
+		} else if (g_error_matches(ierror, R_NBD_ERROR, R_NBD_ERROR_NOT_MODIFIED)) {
+			g_message("polling: bundle not modified");
+			/* TODO update summary? */
+			return TRUE;
+		} else {
+			g_propagate_error(error, ierror);
+			return FALSE;
+		}
+	}
+
+	if (!bundle->manifest) {
+		g_message("polling failed: no manifest found");
+		return FALSE;
+	}
+
+	g_clear_pointer(&poll_source->manifest, free_manifest);
+	poll_source->manifest = g_steal_pointer(&bundle->manifest);
+
+	g_assert(bundle->nbd_srv);
+	poll_source->bundle_size = bundle->nbd_srv->data_size;
+	poll_source->bundle_modified_time = bundle->nbd_srv->modified_time;
+	r_replace_strdup(&poll_source->bundle_effective_url, bundle->nbd_srv->effective_url);
+	r_replace_strdup(&poll_source->bundle_etag, bundle->nbd_srv->etag);
+
+	return TRUE;
+}
+
+static gboolean poll_check_available(RPollSource *poll_source, GError **error)
+{
+	/* decide if we need to install (using system version?) */
+	g_message("polling: system-version=%s bundle-version=%s",
+			r_context()->system_version, poll_source->manifest->update_version);
+
+	/* TODO check install_if */
+
+	if (g_strcmp0(r_context()->system_version, poll_source->manifest->update_version) != 0) {
+		r_replace_strdup(&poll_source->summary, "update available: different system version");
+		return TRUE;
+	}
+
+	r_replace_strdup(&poll_source->summary, "no update available");
+	return FALSE;
+}
+
+static gboolean poll_install_cleanup(gpointer data)
+{
+	RaucInstallArgs *args = data;
+	RPollSource *poll_source = args->data;
+
+	poll_source->installation_running = FALSE;
+
+	g_mutex_lock(&args->status_mutex);
+	if (args->status_result == 0) {
+		g_message("installing `%s` succeeded", args->name);
+	} else {
+		g_message("installing `%s` failed: %d", args->name, args->status_result);
+	}
+	/* TODO expose error? */
+	r_installer_emit_completed(r_installer, args->status_result);
+	r_installer_set_operation(r_installer, "idle");
+	g_dbus_interface_skeleton_flush(G_DBUS_INTERFACE_SKELETON(r_installer));
+	g_mutex_unlock(&args->status_mutex);
+
+	install_args_free(args);
+
+	poll_reschedule(poll_source, POLL_DELAY_SHORT);
+	g_dbus_interface_skeleton_flush(G_DBUS_INTERFACE_SKELETON(r_poller));
+
+	return G_SOURCE_REMOVE;
+}
+
+static gboolean poll_install(RPollSource *poll_source, GError **error)
+{
+	RaucInstallArgs *args = install_args_new();
+	g_autofree gchar *message = NULL;
+	gboolean res;
+
+	g_return_val_if_fail(poll_source, FALSE);
+	g_return_val_if_fail(poll_source->manifest, FALSE);
+	g_return_val_if_fail(poll_source->manifest->hash, FALSE);
+	g_return_val_if_fail(error == NULL || *error == NULL, FALSE);
+
+	args->name = g_strdup(r_context()->config->poll_source);
+	args->cleanup = poll_install_cleanup;
+	args->data = poll_source;
+	/* lock bundle via manifest hash */
+	args->require_manifest_hash = g_strdup(poll_source->manifest->hash);
+
+	r_installer_set_operation(r_installer, "installing");
+	res = install_run(args);
+	if (!res) {
+		message = g_strdup("Failed to launch install thread");
+		args->status_result = 1;
+		goto out;
+	}
+	args = NULL;
+	poll_source->installation_running = TRUE;
+
+out:
+	g_clear_pointer(&args, install_args_free);
+	if (res) {
+	} else {
+		r_installer_set_operation(r_installer, "idle");
+	}
+
+	return TRUE;
+
+	/* TODO trigger installation, which may reboot at the end */
+}
+
+static gboolean poll_step(RPollSource *poll_source, GError **error)
+{
+	GError *ierror = NULL;
+
+	if (!poll_fetch(poll_source, &ierror)) {
+		g_propagate_error(error, ierror);
+		return FALSE;
+	}
+	/* TODO store cache info */
+
+	g_clear_pointer(&poll_source->summary, g_free);
+	poll_source->update_available = FALSE;
+	gboolean available = poll_check_available(poll_source, &ierror);
+	if (ierror) {
+		g_propagate_prefixed_error(error, ierror, "availability check failed: ");
+		return FALSE;
+	} else if (!available) {
+		return TRUE;
+	} else {
+		poll_source->update_available = TRUE;
+	}
+
+	/* retry if manifest hash has changed */
+	if (g_strcmp0(poll_source->manifest->hash, poll_source->attempted_hash) != 0) {
+		r_replace_strdup(&poll_source->attempted_hash, poll_source->manifest->hash);
+		if (!poll_install(poll_source, &ierror)) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
+
 static void poll_update_status(RPollSource *poll_source)
 {
 	g_auto(GVariantBuilder) builder = G_VARIANT_BUILDER_INIT(G_VARIANT_TYPE("a{sv}"));
@@ -859,12 +912,33 @@ static void poll_update_status(RPollSource *poll_source)
 	g_variant_builder_add(&builder, "{sv}", "last-success-time", g_variant_new_uint64(poll_source->last_success_time));
 	if (poll_source->last_error_message)
 		g_variant_builder_add(&builder, "{sv}", "last-error-message", g_variant_new_string(poll_source->last_error_message));
-	if (poll_source->manifest)
-		g_variant_builder_add(&builder, "{sv}", "manifest", r_manifest_to_dict(poll_source->manifest));
 	g_variant_builder_add(&builder, "{sv}", "update-available", g_variant_new_boolean(poll_source->update_available));
 	if (poll_source->summary)
 		g_variant_builder_add(&builder, "{sv}", "summary", g_variant_new_string(poll_source->summary));
-	g_variant_builder_add(&builder, "{sv}", "update-failed", g_variant_new_boolean(poll_source->update_failed));
+	if (poll_source->attempted_hash)
+		g_variant_builder_add(&builder, "{sv}", "attempted-hash", g_variant_new_string(poll_source->attempted_hash));
+
+	if (poll_source->manifest) {
+		/* manifest dict */
+		g_variant_builder_add(&builder, "{sv}", "manifest", r_manifest_to_dict(poll_source->manifest));
+
+		/* bundle dict */
+		g_variant_builder_open(&builder, G_VARIANT_TYPE("{sv}"));
+		g_variant_builder_add(&builder, "s", "bundle");
+		g_variant_builder_open(&builder, G_VARIANT_TYPE("v"));
+		g_variant_builder_open(&builder, G_VARIANT_TYPE("a{sv}"));
+		if (poll_source->bundle_size)
+			g_variant_builder_add(&builder, "{sv}", "size", g_variant_new_uint64(poll_source->bundle_size));
+		if (poll_source->bundle_effective_url)
+			g_variant_builder_add(&builder, "{sv}", "effective-url", g_variant_new_string(poll_source->bundle_effective_url));
+		if (poll_source->bundle_modified_time)
+			g_variant_builder_add(&builder, "{sv}", "modified-time", g_variant_new_uint64(poll_source->bundle_modified_time));
+		if (poll_source->bundle_etag)
+			g_variant_builder_add(&builder, "{sv}", "etag", g_variant_new_string(poll_source->bundle_etag));
+		g_variant_builder_close(&builder); /* inner a{sv} */
+		g_variant_builder_close(&builder); /* inner v */
+		g_variant_builder_close(&builder); /* outer {sv} */
+	}
 
 	r_poller_set_status(r_poller, g_variant_builder_end(&builder));
 }
@@ -918,8 +992,13 @@ static gboolean poll_source_dispatch(GSource *source, GSourceFunc _callback, gpo
 
 	poll_update_status(poll_source);
 
-	/* schedule next poll */
-	poll_reschedule(poll_source, POLL_DELAY_NORMAL);
+	if (poll_source->installation_running) {
+		/* wait until the installation has completed */
+		g_source_set_ready_time(&poll_source->source, -1);
+	} else {
+		/* schedule next poll */
+		poll_reschedule(poll_source, POLL_DELAY_NORMAL);
+	}
 
 	return G_SOURCE_CONTINUE;
 }
@@ -931,6 +1010,9 @@ static void poll_source_finalize(GSource *source)
 	g_clear_pointer(&poll_source->last_error_message, g_free);
 	g_clear_pointer(&poll_source->manifest, free_manifest);
 	g_clear_pointer(&poll_source->summary, g_free);
+	g_clear_pointer(&poll_source->attempted_hash, g_free);
+	g_clear_pointer(&poll_source->bundle_effective_url, g_free);
+	g_clear_pointer(&poll_source->bundle_etag, g_free);
 }
 
 static GSourceFuncs source_funcs = {
