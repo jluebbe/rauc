a07d692dff09e9e6fccb724663fb7301745d29ec
Jan Luebbe <jlu@pengutronix.de>
2025-06-03 18:56:13 +0200

    config: add keyring option for allow-single-signature
    This is supported only if OpenSSL defines CMS_VERIFY_PARTIAL.
    
    Signed-off-by: Jan Luebbe <jlu@pengutronix.de>

--- test/config_file.c
+++ test/config_file.c
@@ -217,14 +217,6 @@ mountprefix=/mnt/myrauc/\n\
 [unknown]\n\
 foo=bar\n\
 ";
-	const gchar *unknown_key_cfg_file = "\
-[system]\n\
-compatible=FooCorp Super BarBazzer\n\
-bootloader=barebox\n\
-mountprefix=/mnt/myrauc/\n\
-foo=bar\n\
-";
-
 	pathname = write_tmp_file(fixture->tmpdir, "unknown_group.conf", unknown_group_cfg_file, NULL);
 	g_assert_nonnull(pathname);
 
@@ -233,9 +225,16 @@ foo=bar\n\
 	g_assert_cmpstr(ierror->message, ==, "Invalid group '[unknown]'");
 	g_clear_error(&ierror);
 
-	g_free(pathname);
+	g_clear_pointer(&pathname, g_free);
 	g_clear_pointer(&config, free_config);
 
+	const gchar *unknown_key_cfg_file = "\
+[system]\n\
+compatible=FooCorp Super BarBazzer\n\
+bootloader=barebox\n\
+mountprefix=/mnt/myrauc/\n\
+foo=bar\n\
+";
 	pathname = write_tmp_file(fixture->tmpdir, "unknown_key.conf", unknown_key_cfg_file, NULL);
 	g_assert_nonnull(pathname);
 
@@ -243,6 +242,26 @@ foo=bar\n\
 	g_assert_error(ierror, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_PARSE);
 	g_assert_cmpstr(ierror->message, ==, "Invalid key 'foo' in group '[system]'");
 	g_clear_error(&ierror);
+
+	g_clear_pointer(&pathname, g_free);
+	g_clear_pointer(&config, free_config);
+
+	if (!ENABLE_OPENSSL_VERIFY_PARTIAL) {
+		const gchar *unsupported_verify_partial_cfg_file = "\
+[system]\n\
+compatible=FooCorp Super BarBazzer\n\
+bootloader=barebox\n\
+[keyring]\n\
+allow-single-signature=true\n\
+";
+		pathname = write_tmp_file(fixture->tmpdir, "unsupported_verify_partial.conf", unsupported_verify_partial_cfg_file, NULL);
+		g_assert_nonnull(pathname);
+
+		g_assert_false(load_config(pathname, &config, &ierror));
+		g_assert_error(ierror, G_KEY_FILE_ERROR, G_KEY_FILE_ERROR_INVALID_VALUE);
+		g_assert_cmpstr(ierror->message, ==, "Keyring section option 'allow-single-signature' is not supported because OpenSSL does not define CMS_VERIFY_PARTIAL");
+		g_clear_error(&ierror);
+	}
 }
 
 static void config_file_bootloaders(ConfigFileFixture *fixture,
